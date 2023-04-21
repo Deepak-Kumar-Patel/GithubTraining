b2dbdd174f3acbd131f573129d57119d799f40e3
Deepak <idd.deepak@gmail.com>
2023-04-21 23:57:53 +0530

    public long add2(long one, long two)

--- src/main/java/com/add/Add.java
+++ src/main/java/com/add/Add.java
@@ -3,7 +3,12 @@ package com.add;
 public class Add {
 
     public long add(long one, long two){
-        long  l = one + two + 11;
+        long  l = one + two + 22;
+        return l;
+    }
+    public long add2(long one, long two){
+        System.out.println(one);
+        long  l = one + two + 22;
         return l;
     }
 }
